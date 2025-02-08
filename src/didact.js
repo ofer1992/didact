478ccb2e18fe43dfe1e6e18845f0ed41185121d9
ofer1992 <geon.arcan@gmail.com>
2025-02-08 15:18:29 +0200

    step 3 done. still scaffolding

--- src/didact.js
+++ src/didact.js
@@ -41,6 +41,25 @@ function render(element, container) {
 
 }
 
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
+}
+
 export default {
   createElement,
   render
